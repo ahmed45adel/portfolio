6375487ad190c9545628a3eb521251445259efa7
Ahmed Adel <ahmed45adel@yahoo.com>
2025-09-15 13:27:35 +0300

    add project sample with interface

---
+++ src/content/projects.ts
@@ -0,0 +1,12 @@
+import { Project } from "@/types";
+
+export const projects: Project[] = [
+    {
+        title: 'E-commerce Platform',
+        description: 'A full-stack e-commerce platform built with Next.js, TypeScript, and Tailwind CSS.',
+        technologies: ['react.js', 'js', 'Tailwind CSS', 'Stripe'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://vercel.com',
+        image: '/public/profile.avif',
+      },
+  ];
